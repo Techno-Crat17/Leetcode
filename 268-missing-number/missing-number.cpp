b3f3ae729cdff4fe965d93378e58355d8b793fb4
Techno-Crat17 <udbhavv27@gmail.com>
2026-07-25 12:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (5.36%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int xor1=0,xor2=0;
+        int n=nums.size();
+        vector<int> hash(n+1,0);
+
         for(int i=0;i<nums.size();i++){
-            xor1^=nums[i];
-            xor2^=(i);
+            hash[nums[i]]=1;
         }
-        xor2^=nums.size();
-        int ans=xor1^xor2;
-        return ans;        
+
+        for(int i=0;i<n+1;i++){
+           if( hash[i]==0) return i;
+        }
+        return -1;
     }
 };
