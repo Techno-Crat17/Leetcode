80c04f045e5d9cc888e6d7a6eda37b471cdc5a47
Techno-Crat17 <udbhavv27@gmail.com>
2026-07-27 17:44:25 +0530

    Time: 3 ms (7.46%) | Memory: 13.1 MB (91.55%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -4,22 +4,18 @@ public:
         int j=0,i=0;
         int n=nums.size();
 
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
+        for(int i=0;i<nums.size();i++){
+            if(i<n-1 && nums[i]==nums[i+1]){
                 nums[i]=nums[i]*2;
                 nums[i+1]=0;
             }
+
+            if(nums[i]!=0){
+                swap(nums[j++],nums[i]);
+            }
+
         }
         
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                nums[j++]=nums[i];
-            }
-        }
-        while(j<nums.size()){
-            nums[j++]=0;
-        }
-
         return nums;
     }
 };
