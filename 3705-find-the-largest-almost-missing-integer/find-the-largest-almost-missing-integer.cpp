12428d2b9ce821f9596d6c959bb77b655322ba76
Udbhav Verma <udbhavv27@gmail.com>
2026-08-18 08:40:59 +0530

    Time: 7 ms (35.34%) | Memory: 29.3 MB (56.02%) - LeetSync

---
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+       
+        for(int x:nums) mp[x]++;
+
+        if(k==1){//first ele
+            int ans=-1;
+
+            for(auto &[x,f]:mp){
+                if(f==1) ans=max(ans,x);
+            }
+            return ans;
+        }
+
+        if(k==nums.size()){//whole window
+            return *max_element(nums.begin(),nums.end());
+        }
+
+        //any ele other first and last always appers more than 1 times in subarrays
+        int ans=-1;
+        int firstcnt=count(nums.begin(),nums.end(),nums[0]);//counting freq of nums[0]
+        if(firstcnt==1) ans=max(ans,nums[0]);
+
+        int lastcnt=count(nums.begin(),nums.end(),nums[nums.size()-1]);//counting freq of nums[n-1]
+        if(lastcnt==1) ans=max(ans,nums[nums.size()-1]);
+
+        return ans;
+
+        
+    }
+};
