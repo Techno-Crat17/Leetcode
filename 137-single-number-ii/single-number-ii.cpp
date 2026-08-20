736965945fc8f629595b64053bdd31bafc5f5900
Udbhav Verma <udbhavv27@gmail.com>
2026-08-20 14:30:08 +0530

    Time: 3 ms (32.03%) | Memory: 13.4 MB (35.16%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int ans= 0;
 
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
+        int ans=0;
+        int cnt=0;
+        for(int bidx=0;bidx<32;bidx++){//for every bit
+            cnt=0;
+            for(int i=0;i<nums.size();i++){//checking(set ) cnt every no at particular bit
+                if(nums[i] & (1<<bidx)) cnt++;
+            }
 
-        for(auto it:mp){
-            if(it.second==1) ans=it.first;
+            if(cnt%3==1){ //setting that bit isnt multiple of 3
+                ans=ans | (1<<bidx);
+
+            }
+
         }
         return ans;
     }
