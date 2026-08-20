56d0ddb9fcddde9a58e62d43e163c3f72ba7a085
Udbhav Verma <udbhavv27@gmail.com>
2026-08-20 14:32:55 +0530

    Time: 3 ms (32.03%) | Memory: 13.3 MB (66.17%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
 
-        int ans=0;
-        int cnt=0;
-        for(int bidx=0;bidx<32;bidx++){//for every bit
-            cnt=0;
-            for(int i=0;i<nums.size();i++){//checking(set ) cnt every no at particular bit
-                if(nums[i] & (1<<bidx)) cnt++;
-            }
-
-            if(cnt%3==1){ //setting that bit isnt multiple of 3
-                ans=ans | (1<<bidx);
-
-            }
-
+        for(int i=1;i<n;i+=3){
+            if(nums[i]!=nums[i-1]) return nums[i-1];
         }
-        return ans;
+        return nums[n-1];
+        
     }
 };
