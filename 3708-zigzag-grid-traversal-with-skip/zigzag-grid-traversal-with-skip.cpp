b633947638006abbe8ca6fcacb01992d61d8e4c9
Udbhav Verma <udbhavv27@gmail.com>
2026-08-22 19:39:32 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (62.16%) - LeetSync

--- 3708-zigzag-grid-traversal-with-skip/zigzag-grid-traversal-with-skip.cpp
+++ 3708-zigzag-grid-traversal-with-skip/zigzag-grid-traversal-with-skip.cpp
@@ -4,24 +4,25 @@ public:
 
         int m=grid.size();
         int n=grid[0].size(); 
-        bool take=true;//we can use modulo also for 0 1 0 1 pattern
+        int dir=0;//we can use modulo also for 0 1 0 1 pattern
         vector<int> ans;
         for(int i=0;i<m;i++){
             if(i%2==0){
                 for(int j=0;j<n;j++){
-                    if(take)
+                    if(dir==0)
                 ans.push_back(grid[i][j]);
 
-                take=!take;
+                dir=(dir+1)%2;//0 1 0 1
                 }
             }
 
             else{
                 for(int j=n-1;j>=0;j--){
-                    if(take)
+                    if(dir==0)
                 ans.push_back(grid[i][j]);
 
-                take=!take;
+                
+                dir=(dir+1)%2;//0 1 0 1 and, we can generate for take skip skip if we 0 1 1 0 like this etc pattern
                 }
             }
         }
