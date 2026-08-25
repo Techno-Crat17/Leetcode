d2f747f9374bcd2f0c1c7e0d35d0ace9d5b1036b
Udbhav Verma <udbhavv27@gmail.com>
2026-08-25 21:21:07 +0530

    Time: 108 ms (5.12%) | Memory: 65.1 MB (5.85%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+
+        map<int,int> mp;
+        vector<int> freq(10, 0);
+
+        for (int x : digits)
+            freq[x]++;
+
+        for (int i = 100; i <= 998; i += 2) {
+            // even no
+            string s = to_string(i);//easy access to digit
+
+            vector<int> need(10, 0);
+
+            for (char c : s) {
+                need[c - '0']++;
+            }
+
+            bool possible = true;
+            for(int d=0;d<10;d++){
+                if(need[d]>freq[d]) {
+                    possible=false; // no cant bed formed as needed digits more
+                    break;
+                }
+            }
+            if(possible ) mp[i]++;
+        }
+    
+        return mp.size();
+    }
+};
