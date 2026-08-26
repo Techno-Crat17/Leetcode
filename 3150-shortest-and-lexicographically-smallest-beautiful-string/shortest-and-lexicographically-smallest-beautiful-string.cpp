69abdaac54b61af9097023ed969e6f7cc8ca7e53
Udbhav Verma <udbhavv27@gmail.com>
2026-08-26 09:53:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (95.97%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
+        string ans = "";
+        int ones = 0;
+        int l = 0;
 
-        string ans="";
-        
-         for(int i=0;i<s.length();i++){
-            int ones=0;
-            for(int j=i;j<s.length();j++){
-                if(s[j]-'0'==1) ones++;
+        for (int r = 0; r < s.size(); r++) {
+            if (s[r] == '1')
+                ones++;
 
-                if(ones==k){
-                    string cur=s.substr(i,j-i+1);
+            
+            while (ones > k) {// all valid window checks
+                if (s[l] == '1')
+                    ones--;
+                l++;
+            }
 
-                    if(ans.empty() || cur.size()<ans.size() || (cur.size()==ans.size() && cur<ans)) ans=cur;
+           
+            if (ones == k) {
+                // Remove leading zeroes
+                while (s[l] == '0')
+                    l++;
+
+                string cur = s.substr(l, r - l + 1);
+
+                if (ans.empty() ||
+                    cur.size() < ans.size() ||
+                    (cur.size() == ans.size() && cur < ans)) {
+                    ans = cur;
                 }
-
-                if(ones > k) break;
             }
         }
+
         return ans;
     }
 };
