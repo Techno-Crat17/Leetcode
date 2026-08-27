c8f7b3aee00cf02e55564adc41980378e61241e0
Udbhav Verma <udbhavv27@gmail.com>
2026-08-27 19:15:03 +0530

    Time: 3 ms (52.17%) | Memory: 11.8 MB (74.38%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int n=ransomNote.size();
-        int m=magazine.size();
-        unordered_map<char,int> mp;//saving magazine char,freq 
-        // if ransom can be formed that means it must equal freq char or more
-        for(int i=0;i<m;i++){
-            mp[magazine[i]]++;
+        int freq[26]={0};
+
+        for(char c:magazine) freq[c-'a']++;
+
+        for(char c:ransomNote){
+            if(freq[c-'a']==0) return false;//ele missing when req
+            freq[c-'a']--;
         }
 
-        for(int i=0;i<n;i++){
-            char ch=ransomNote[i];
-            if(mp.find(ch)==mp.end() || mp[ch]==0){//if that char not availab or became empty(still req so false)
-               return false;
-            }
-           mp[ch]--;
-        }
-     
-         return true;
+        return true;
     }
 };
