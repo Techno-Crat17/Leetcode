3a443611db6f2bf118ee05d2089179eb1c63b0cd
Udbhav Verma <udbhavv27@gmail.com>
2026-09-01 20:14:58 +0530

    Time: 3 ms (61.57%) | Memory: 18.5 MB (48.61%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -11,24 +11,28 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int,vector<int>,greater<int>> pq;
+       priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>pq;
 
         for( auto head:lists){
-            while(head){
-            pq.push(head->val);
-            head=head->next;
-            }
+            if(head!=nullptr) pq.push({head->val,head});
         }
 
+        // we r checking list node wise
         
         ListNode dummy(-1);
         ListNode * tail=&dummy;
 
         while(!pq.empty()){
-            int val=pq.top(); pq.pop();
+            auto [val,node] =pq.top(); pq.pop();
 
-            tail->next=new ListNode (val);
+            tail->next=node;
             tail=tail->next;
+
+            if(node->next!=nullptr){//agar same list ka node insertion
+                pq.push({node->next->val,node->next});
+            }
+            //agar ek list khatam ho jaye ,lekin we have saved head of reamining list
+            
         }
 
         return dummy.next;
