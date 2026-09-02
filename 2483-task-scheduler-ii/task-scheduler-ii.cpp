21cafd3e3799663022939934484cd71b33f6b18d
Udbhav Verma <udbhavv27@gmail.com>
2026-09-02 22:55:28 +0530

    Time: 62 ms (57.89%) | Memory: 106.1 MB (17.98%) - LeetSync

--- 2483-task-scheduler-ii/task-scheduler-ii.cpp
+++ 2483-task-scheduler-ii/task-scheduler-ii.cpp
@@ -3,21 +3,18 @@ class Solution {
 public:
     long long taskSchedulerII(vector<int>& tasks, int space) {
         long long currentDay = 0;
-        unordered_map<int, long long> lastExecutedDay;
+        unordered_map<int, long long> last;
 
         for (int task : tasks) {
             currentDay++;
 
             // Agar ye task humne pehle bhi kiya hai, toh cooldown check karo
-            if (lastExecutedDay.count(task)) {
-                long long nextAvailableDay = lastExecutedDay[task] + space + 1;
-                
-                // Agar currentDay piche rehar gaya hai cooldown requirement se, 
-                // toh currentDay ko directly agle valid day par jump karwa do
-                currentDay = max(currentDay, nextAvailableDay);
+            if (last.count(task) && currentDay<=last[task]+space) {
+               currentDay = last[task] + space + 1;
+            
             }
 
-            lastExecutedDay[task] = currentDay;
+            last[task] = currentDay;
         }
 
 
