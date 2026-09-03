163732eb73d74e681322f8c263d4130b5c205829
Udbhav Verma <udbhavv27@gmail.com>
2026-09-04 00:54:00 +0530

    Time: 53 ms (5.33%) | Memory: 24.4 MB (7.11%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -4,22 +4,25 @@ public:
 
         int n=matrix.size();
 
-        priority_queue<
-            int,vector<int>,greater<int>
-            > pq;
-
-       for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                pq.push(matrix[i][j]);
-            }
+    priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+       for (int r = 0; r < n; r++) {
+                //as we know the rows are sorted we taking first ele at start
+                pq.push({matrix[r][0],r,0});
+            
         }
 
         while(--k){
            
+            auto top=pq.top();
             pq.pop();
+
+
+            int r=top[1];
+            int c=top[2];
+            if(c+1<n )  pq.push({matrix[r][c+1],r,(c+1)});//dynamic inc sizing  as it will inc efficeiy
 
            
         }
-        return pq.top()  ;
+        return pq.top() [0] ;
     }
 };
