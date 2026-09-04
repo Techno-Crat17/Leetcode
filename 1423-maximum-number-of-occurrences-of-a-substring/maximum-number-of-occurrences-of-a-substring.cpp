d4a0cbcfbb6ea91b42241b0e73b804a00048f03e
Udbhav Verma <udbhavv27@gmail.com>
2026-09-05 02:48:46 +0530

    Time: 63 ms (68.48%) | Memory: 22.8 MB (59.87%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.cpp
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.cpp
@@ -3,21 +3,19 @@ public:
     int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
         int l=0,r=0;
         int ans=0;
-        int distinct=0;
         unordered_map<string,int> mp;
         unordered_map<char,int> mps;
         while(r<s.size()){
-            if(mps[s[r]]==0) distinct++;
 
             mps[s[r]]++;
 
             while(r-l+1>minSize){
                 mps[s[l]]--;
-                if(mps[s[l]]==0) distinct--;
+                if(mps[s[l]]==0) mps.erase(s[l]);
                 l++;
             }
 
-            if(r-l+1==minSize && distinct<=maxLetters){
+            if(r-l+1==minSize && mps.size()<=maxLetters){
                 string sub=s.substr(l,r-l+1);
                 mp[sub]++;
                 ans=max(ans,mp[sub]);
