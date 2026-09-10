8ade017c8088203d6c9e1d1362caf8517aab77e2
Udbhav Verma <udbhavv27@gmail.com>
2026-09-10 21:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (18.08%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     bool checkString(string s) {
 
-        string f="ba";
-
-        if(s.contains(f)) return false;
-
+       for (int i = 0; i < s.length() - 1; i++) {
+            if (s[i] == 'b' && s[i + 1] == 'a') {
+                return false;
+            }
+        }
         return true;
         
     }
