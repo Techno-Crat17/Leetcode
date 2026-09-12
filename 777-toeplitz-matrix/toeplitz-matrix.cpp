d97480db9b3de0ea38a92806c130544b43fbe654
Udbhav Verma <udbhavv27@gmail.com>
2026-09-12 16:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (89.01%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.cpp
+++ 777-toeplitz-matrix/toeplitz-matrix.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+    bool isToeplitzMatrix(const vector<vector<int>>& matrix) {
+        const int m = matrix.size();
+        const int n = matrix[0].size();
 
-        int m=matrix.size();
-        int n=matrix[0].size();
+        //diagonals starting from the first column
+        for (int r = 0; r < m; ++r) {
+            if (!isValidDiagonal(matrix, r, 0, m, n)) {
+                return false;
+            }
+        }
 
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if (matrix[i][j]!=matrix[i-1][j-1]) return false;
+        //diagonals starting from the first row 
+        for (int c = 1; c < n; ++c) {
+            if (!isValidDiagonal(matrix, 0, c, m, n)) {
+                return false;
             }
         }
 
         return true;
-        
+    }
+
+private:
+    bool isValidDiagonal(const vector<vector<int>>& matrix, int r, int c, int m, int n) {
+        const int val = matrix[r][c];
+        while (r < m && c < n) {
+            if (matrix[r][c] != val) {//00 11 22 etc //01 12 aise diagonal move karenge
+                return false;
+            }
+            ++r;
+            ++c;
+        }
+        return true;
     }
 };
