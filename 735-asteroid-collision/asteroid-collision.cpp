1aa9bd03521df6576cfb53093472378168e352da
Udbhav Verma <udbhavv27@gmail.com>
2026-09-14 19:21:12 +0530

    Time: 3 ms (27.84%) | Memory: 22.2 MB (17.45%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,56 +1,41 @@
-class Solution { 
-public: 
-    vector<int> asteroidCollision(vector<int>& asteroids) { 
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
 
-        stack<pair<int,int>> st; 
+        int i=0;
+        int n=asteroids.size();
+        stack<int>st;
+        while(i<n){
 
-        int n = asteroids.size();
+            //no collision cases
+            if(st.empty() || st.top()<0 || asteroids[i]>0){
+                st.push(asteroids[i]);//asteroids tab hi push hoga ye teeno condition hogi
+                i++;
+            }
 
-        for(int i = 0; i < n; i++) {
+            else if(!st.empty() && st.top()<-asteroids[i]){
+                st.pop();//remove only top and we push inc later
+            }
 
-            int flag;
-
-            if(asteroids[i] > 0)
-                flag = 1;
-            else
-                flag = -1;
-
-            bool destroyed = false;
-
-            while(!st.empty() && st.top().second == 1 && flag == -1) {
-
-                auto x = st.top();
+            else if(!st.empty() && st.top()==-asteroids[i]){
                 st.pop();
-
-                // Stack asteroid is bigger
-                if(x.first > -asteroids[i]) {
-                    st.push(x);
-                    destroyed = true;
-                    break;
-                }
-
-                // Both are same size
-                else if(x.first == -asteroids[i]) {
-                    destroyed = true;
-                    break;
-                }
-
+                i++;//incoming asteroid destroyed
             }
 
-            if(!destroyed) {
-                st.push({asteroids[i], flag});
-            }
+        
+        else    i++;
+
         }
 
         vector<int> ans;
-
         while(!st.empty()) {
-            ans.push_back(st.top().first);
+            ans.push_back(st.top());
             st.pop();
         }
 
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
 
         return ans;
+        
     }
 };
